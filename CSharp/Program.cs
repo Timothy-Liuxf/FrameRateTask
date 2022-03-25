4be826732cf7df0529e35a4741552a6be1b6830d
timothy <liuxf19@163.com>
2022-03-25 20:29:46 +0800

    fix: data race when used in concurrent environment

---
+++ CSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿// using Timothy.FrameRateTask;
+
+
+
+namespace TestNullable
+{
+	class Foo<T>
+	{
+		public T val;
+
+		public Foo(T val)
+		{
+			// this.val = val;
+		}
+	}
+
+	static class Program
+	{
+		public static void Main()
+		{
+
+		}
+	}
+}
