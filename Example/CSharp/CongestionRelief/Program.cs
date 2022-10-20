062904f350d0e962c2471f95e8fc42f48ca60b7a
Timothy Liu <liuxf19@163.com>
2022-10-20 23:45:58 +0800

    refactor: :recycle: move some test code to example code
    Move test code in `CSharp/Test` to `Example/CSharp` as example code

---
+++ Example/CSharp/CongestionRelief/Program.cs
@@ -0,0 +1,40 @@
+﻿using Timothy.FrameRateTask;
+
+Random r = new Random();
+int tm = 0;
+FrameRateTaskExecutor<int> executor = new FrameRateTaskExecutor<int>
+    (
+        () => true,
+        () =>
+        {
+            tm = r.Next(0, 280);    // Time consuming, but can finish the task at most time.
+            Console.WriteLine($"Loop once! Sleep time: {tm}; At tickcount: {Environment.TickCount64}");
+            Thread.Sleep(tm);
+        },
+        200,
+        () => 0,
+        maxTotalDuration: 10000
+    )
+{
+    AllowTimeExceed = true,
+    MaxTolerantTimeExceedCount = ulong.MaxValue,
+    TimeExceedAction = b =>
+    {
+        if (b)
+        {
+            Console.WriteLine("Reset!");
+        }
+    }
+};
+Task.Run
+    (
+        () =>
+        {
+            while (!executor.Finished)
+            {
+                Console.WriteLine($"Now framerate: {executor.FrameRate}");
+                Thread.Sleep(1000);
+            }
+        }
+    );
+executor.Start();
---
+++ Example/CSharp/Hello/Program.cs
@@ -0,0 +1,32 @@
+﻿using Timothy.FrameRateTask;
+
+Random r = new Random();
+int cnt = 0;
+FrameRateTaskExecutor<int> executor = new FrameRateTaskExecutor<int>
+    (
+        () => true,
+        () =>
+        {
+            Console.WriteLine($"Hello! (The {++cnt}-th frame. At TickCount: {Environment.TickCount64})");
+        },
+        50,
+        () => { return 0; },
+        maxTotalDuration: 2000
+    )
+{
+    AllowTimeExceed = true,
+    TimeExceedAction = b => { Console.WriteLine(b ? "Reset time!" : "Time exceed for once!"); },
+    MaxTolerantTimeExceedCount = 5      // If time exceeds
+};
+Task.Run
+    (
+        () =>
+        {
+            while (!executor.Finished)
+            {
+                Console.WriteLine($"Now framerate: {executor.FrameRate}");
+                Thread.Sleep(1000);
+            }
+        }
+    );
+executor.Start();
